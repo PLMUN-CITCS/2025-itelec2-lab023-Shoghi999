2a6b4b3727a42e3740cbfeb7803b2c237f9b9115
Shoghi999 <marcoshoghiindycyril_bsit@plmun.edu.ph>
2025-02-22 22:46:14 +0800

    Submitting Python Week 05 - Laboratory # 23

--- word_counter_functions.py
+++ word_counter_functions.py
@@ -4,6 +4,10 @@ def get_sentence_input():
 def count_words(sentence):
     return len(sentence.split())
 
-sentence = get_sentence_input()
-word_count = count_words(sentence)
-print(f"The sentence has {word_count} words.")
+sentence1 = get_sentence_input()
+word_count1 = count_words(sentence1)
+print(f"The sentence has {word_count1} words.\n")
+
+sentence2 = get_sentence_input()
+word_count2 = count_words(sentence2)
+print(f"The sentence has {word_count2} words.")
